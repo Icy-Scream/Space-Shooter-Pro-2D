381b475b5eadba6aa849f8eb65753ded383d85fd
Icy Screams <jdpetta21@gmail.com>
2022-10-09 23:29:58 -1000

    Player Model

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.right* 5 * Time.deltaTime);
+    }
+}
