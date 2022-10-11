149944040e0a518d776146d62a11bc9b62e13602
Icy Screams <jdpetta21@gmail.com>
2022-10-10 20:41:22 -1000

    Catching up,Bank CS, Player Movement

--- Assets/Scripts/PlayerFPS.cs
+++ Assets/Scripts/PlayerFPS.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerFPS: MonoBehaviour
 {
     [SerializeField ] private float _playerSpeed = 5.0f;
     
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0,0,0);
+       
     }
 
     // Update is called once per frame
@@ -17,7 +17,7 @@ public class Player : MonoBehaviour
     {
          PlayerBoundaries();
         _playerMovement();
-        transform.Translate(new Vector3(1,0,0)*300* Time.deltaTime);
+        transform.Translate(new Vector3(1,0,0));
         
     }
     
