ad0acd282a7b704aaab692bdefc98a2c9f8fb002
Icy Screams <jdpetta21@gmail.com>
2022-10-10 21:41:16 -1000

    Laser Script: Laser Translation,Destroy Laser Object

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private float _laserSpeed;
+    [SerializeField] private float _destroyTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestroyLaserRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LaserTranslate();
+    }
+
+    private void LaserTranslate()
+    {
+        transform.Translate(Vector3.up*_laserSpeed*Time.deltaTime);
+    }
+
+    IEnumerator DestroyLaserRoutine()
+    {
+        yield return new WaitForSeconds(_destroyTimer);
+        Object.Destroy(this.gameObject);
+    }
+}
