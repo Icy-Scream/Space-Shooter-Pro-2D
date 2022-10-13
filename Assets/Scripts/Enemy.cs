f6977a74fab4a8370043e590c48d989df1f1e67f
Icy Screams <jdpetta21@gmail.com>
2022-10-12 14:06:47 -1000

    Added Enemy Movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Vector3 _enemyDirection = new Vector3(0,-1,0);
+    [SerializeField] private float _enemySpawn = 6.8f;
+    [SerializeField] private Vector3 _randomSpawn;
+    [SerializeField] private float _enemySpeed = 4.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyMovement();
+    }
+
+    private void EnemyMovement()
+    {
+        transform.Translate((_enemyDirection) *_enemySpeed * Time.deltaTime);
+        if(transform.position.y < -6.5f )
+        {
+            _randomSpawn = new Vector3(Random.Range(-11.18f,11.18f),_enemySpawn,0);
+            transform.position =_randomSpawn;
+        } 
     }
 }
