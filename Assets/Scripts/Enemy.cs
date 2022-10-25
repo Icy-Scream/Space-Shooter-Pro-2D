edce993b684931f22c09253e0298406295c77329
Icy Screams <jdpetta21@gmail.com>
2022-10-24 16:40:58 -1000

    Created Explosion Animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private Vector3 _enemyDirection = new Vector3(0,-1,0);
+    [SerializeField] private Vector3 _enemyDirection = new Vector3(0, -1, 0);
     [SerializeField] private float _enemySpawn = 6.8f;
     [SerializeField] private Vector3 _randomSpawn;
     [SerializeField] private float _enemySpeed = 4.0f;
-    private Player  _player;
+    [SerializeField] private GameObject _explosion;
+    private Player _player;
 
     private void Start()
     {
@@ -18,55 +18,63 @@ public class Enemy : MonoBehaviour
             _player = GameObject.Find("Player").GetComponent<Player>();
         }
         else Debug.Log("Player Object Destroyed");
-       
+
     }
     void Update()
     {
-        EnemyMovement();    
+        EnemyMovement();
     }
 
     private void EnemyMovement()
     {
-        transform.Translate((_enemyDirection) *_enemySpeed * Time.deltaTime);
-        if(transform.position.y < -6.5f )
+        transform.Translate((_enemyDirection) * _enemySpeed * Time.deltaTime);
+        if (transform.position.y < -6.5f)
         {
-            _randomSpawn = new Vector3(Random.Range(-11.18f,11.18f),_enemySpawn,0);
-            transform.position =_randomSpawn;
-        } 
+            _randomSpawn = new Vector3(Random.Range(-11.18f, 11.18f), _enemySpawn, 0);
+            transform.position = _randomSpawn;
+        }
     }
 
-private void OnTriggerEnter2D(Collider2D other) 
-{
-    
-  
-    
-    if(other.gameObject.tag == "Laser")
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if(_player != null) 
-            { 
-            
-                _player.AddScore(Random.Range(0,50));
+
+
+
+        if (other.gameObject.tag == "Laser")
+        {
+            if (_player != null)
+            {
+                _enemySpeed = 0;
+                _player.AddScore(Random.Range(0, 50));
+                ExplosionEffect();
                 Destroy(other.gameObject);
-                Destroy(this.gameObject);
+                Destroy(this.gameObject,0.5f);
             }
             else
             {
                 Debug.Log("Missing Player Object");
             }
-    }
-            
-            
-    else if(other.gameObject.tag == "Player")
-    {
-        if(_player != null)
-        {
-           Destroy(this.gameObject);
-           _player.Damage();
         }
-        else
-        Debug.Log("COMPONENT MISSING");
-            
+
+
+        else if (other.gameObject.tag == "Player")
+        {
+            if (_player != null)
+            {
+                _enemySpeed = 0;
+                ExplosionEffect();
+                Destroy(this.gameObject,0.5f);
+                _player.Damage();
+            }
+            else
+                Debug.Log("COMPONENT MISSING");
+
+        }
     }
-}
+
+    private void ExplosionEffect() 
+    {
+        Instantiate(_explosion, this.transform.position, Quaternion.identity);
+    }
 
 }
