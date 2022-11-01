b944e0962b1431dbc2cee0f95aa5e8220df9cc4e
Icy-Scream <jdpetta21@gmail.com>
2022-10-31 14:55:06 -1000

    Added two Wave Systems.Deciding on Which one

--- Assets/Scripts/Spawn_Mananger.cs
+++ Assets/Scripts/Spawn_Mananger.cs
@@ -11,14 +11,16 @@ public class Spawn_Mananger : MonoBehaviour
     [SerializeField] float _rarePowerUpTimer;
     [SerializeField] float _powerUpTimer;
     [SerializeField] private bool _stopspawing = false;
-    [SerializeField] GameObject _waves;
+    [SerializeField] GameObject[] _waves;
+    [SerializeField] private int _amountSpawnPerWave = 1;
+    GameObject[] _enemyWave;
 
     private Vector3 _spawnPOS;
 
     public void StartSpawning()
     {
-        StartCoroutine(SpawnWaveRoutine());
-        //StartCoroutine(SpawnEnemyRoutine());
+        //StartCoroutine(SpawnWaveRoutine());
+        StartCoroutine(SpawnEnemyRoutine());
         StartCoroutine(SpawnPowerUp());
         StartCoroutine(RareSpawnPowerUp());
     }
@@ -29,29 +31,44 @@ public class Spawn_Mananger : MonoBehaviour
         while (_stopspawing == false)
         {
             yield return new WaitForSeconds(_spawnEnemyTimer);
-            _spawnPOS = new Vector3(Random.Range(-9.0f, 9.0f), 6.8f, 0);
-            GameObject newEnemy = Instantiate(_enemy, _spawnPOS, Quaternion.identity);
-            newEnemy.transform.parent = _enemyContainer.transform;
-
-        }
-
-    }
-
-    IEnumerator SpawnWaveRoutine()
-    {
-        GameObject[] _enemyWave = _waves.GetComponent<Waves>().GetEnemies();
-        yield return new WaitForSeconds(3);
-        while (_stopspawing == false)
-        {
-            yield return new WaitForSeconds(_spawnEnemyTimer);
-            _spawnPOS = new Vector3(Random.Range(-9.0f, 9.0f), 6.8f, 0);
-            for(int k = 0; k < _enemyWave.Length; k++)
-            {
-                GameObject newEnemy = Instantiate(_enemyWave[k], _spawnPOS, Quaternion.identity);
+            for(int i = 0; i < _amountSpawnPerWave; i++) 
+            { 
+                 _spawnPOS = new Vector3(Random.Range(-9.0f, 9.0f),Random.Range(5.5f,9.0f), 0);
+                GameObject newEnemy = Instantiate(_enemy, _spawnPOS, Quaternion.identity);
                 newEnemy.transform.parent = _enemyContainer.transform;
             }
+            _amountSpawnPerWave++;
         }
+
+
+
+
     }
+
+        
+    IEnumerator SpawnWaveRoutine()
+    {
+        yield return new WaitForSeconds(3);
+        for(int i = 0; i < _waves.Length; i++) 
+        { 
+            _enemyWave = _waves[i].GetComponent<Waves>().GetEnemies();
+        
+            yield return new WaitForSeconds(_spawnEnemyTimer);
+            for(int k = 0; k < _enemyWave.Length; k++)
+             {
+               _spawnPOS = new Vector3(Random.Range(-9.0f, 9.0f), 6.8f, 0);
+               GameObject newEnemy = Instantiate(_enemyWave[k], _spawnPOS, Quaternion.identity);
+               newEnemy.transform.parent = _enemyContainer.transform;
+             }
+            
+        }
+        Debug.Log("EXIT FOR LOOP");
+    }
+        
+        
+        
+        
+       
             
 
 
